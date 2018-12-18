59398853bd5566cb1bc7066e3dc8c72b9858236c
user1-pc\user1 <h3711220@gmail.com>
2018-12-18 15:29:11 +0900

    learning

--- Insincere_problem/Char_CNN/model.py
+++ Insincere_problem/Char_CNN/model.py
@@ -1,10 +1,16 @@
+#Character-Aware Neural Language Models
+
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 from keras.layers import Input, Embedding, Conv1D, GlobalMaxPooling1D, Concatenate, Dense, Lambda, add, multiply, TimeDistributed, LSTM, Dropout, Flatten
 from keras.models import Model
 from keras import Sequential
 from keras import backend as K
+from keras.optimizers import SGD
+from utils import DataLoader, generator
 
+
+path = r'C:\Users\user1\Desktop\Kaggle\Insincere_problem\Input'
 
 class embedding(object):
 
@@ -26,6 +32,8 @@ class embedding(object):
     def get(self):
 
         inputs = Input(shape=(self.word_max_len,))
+
+        #Embedding
         x = Embedding(self.max_features, self.embed_size, input_shape=(self.word_max_len,))(inputs) # shape: batch_size, max_len, emb_size
 
         feature_maps = []
@@ -38,10 +46,13 @@ class embedding(object):
 
         max_pools=[]
 
+        #Max over time pooling layer
+
         for i in range(len(feature_maps)):
             max_pool = GlobalMaxPooling1D(name='Maxovertimepoolinglayer_{}'.format(i))(feature_maps[i])
             max_pools.append(max_pool)
 
+        #High way
         feature_vectors = Concatenate(axis=1)(max_pools)
 
         transform_gate = Dense(525, activation='sigmoid',name='transform_gate', use_bias=True)(feature_vectors)
@@ -56,10 +67,11 @@ class embedding(object):
 
 class CharRNN(object):
 
-    def __init__(self, num_unit):
+    def __init__(self, num_unit, word_index):
 
         self.num_unit = num_unit
         self.max_len = 35
+        self.word_index = word_index
         self.word_max_len = 20
 
     def get(self, embedding):
@@ -70,11 +82,25 @@ class CharRNN(object):
         model.add(Dropout(0.5))
         model.add(LSTM(self.num_unit, return_sequences=True))
         model.add(Dropout(0.5))
-        model.add(Flatten())
-        model.add(Dense(1, activation='sigmoid'))
+        model.add(TimeDistributed(Dense(len(self.word_index), activation='linear')))
 
         return model
 
+def PPL(y_true, y_pred):
+    #エントロピー Hは，底 2 の負の対数尤度を単語数 |Wtest| で割った値で次式で表される．エントロピーが大きいほど次の単語の予測が困難（不確実）である．
+    #パープレキシティ (Perplexity) PPL は， 2 のエントロピー乗．この値が小さいほど優れたモデルだといえる．
+    return K.pow(2.0, K.mean(K.categorical_crossentropy(y_true, y_pred, from_logits=True)))
+
+def categorical_crossentropy(y_true, y_pred):
+    return K.categorical_crossentropy(y_true, y_pred,from_logits=True )
+
+DataLoader = DataLoader(path, max_len=30)
+word_index, char_index = DataLoader.word_index, DataLoader.char_index
+inputs_tr, outputs_tr = DataLoader.inputs, DataLoader.outputs
+
 embedding = embedding(num_filter=[25, 50, 75, 100, 125, 150], kernel_size=[1, 2, 3, 4, 5 ,6]).get()
-model = CharRNN(num_unit=300).get(embedding)
+model = CharRNN(num_unit=300, word_index=word_index).get(embedding)
 model.summary()
+model.compile(optimizer=SGD(lr=1.0), loss=categorical_crossentropy, metrics=['accuracy', PPL])
+gen = generator(inputs_tr, outputs_tr, batch_size=20, max_len=35, max_word_len=20, word_index=word_index, char_index=char_index)
+model.fit_generator(gen, steps_per_epoch=len(inputs_tr)//35//20, epochs=25, verbose=1) 
--- Insincere_problem/Char_CNN/utils.py
+++ Insincere_problem/Char_CNN/utils.py
@@ -1,21 +1,117 @@
-from keras.preprocessing.text import text_to_word_sequence
 import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import re
+import numpy as np
 
 path = r'C:\Users\user1\Desktop\Kaggle\Insincere_problem\Input'
+max_len = 35
 
-csv_path_train = os.path.join(path, 'train.csv')
-csv_data_train = pd.read_csv(csv_path_train)
-train_data, val_data = train_test_split(csv_data_train, test_size=0.1, random_state=2018)
+class DataLoader(object):
 
-train_X = train_data["question_text"].fillna("_na_").values
-val_X = val_data["question_text"].fillna("_na_").values
-train_y = train_data['target'].values
-val_y = val_data['target'].values
+    def __init__(self, path, max_len):
+        self.path = path
+        self.max_len = max_len
+        self.data_path = os.path.join(path, 'small_vocab_en')
 
-train_X_tokens = []
+        self.train_data = self._load_data(self.data_path)
 
-for text in train_X:
-    train_X_tokens.append([text_to_word_sequence(text)])
+        text = []
+        for sequence in self.train_data:
+            [text.append(word) for word in sequence]
+        self.word_training = list(set(text))
+        self.char_training = []
+        #Convert $w_t$ into a series of characters: $w_t = [c_1, c_2, ..., c_N]$
+        for word in self.word_training:
+            for char in word:
+                if char not in self.char_training:
+                    self.char_training.append(char)
 
+        self.word_index, self.char_index = self._create_dict(self.word_training, self.char_training)
+        self.input_chars, self.output_words = self.prepare_input_output(self.train_data)
+        self.inputs, self.outputs = self.create_input_output(self.input_chars, self.output_words)
+
+    def _load_data(self, path):
+
+        f = open(path, 'r')
+        f = [sequence.replace('\n', ' ') for sequence in f]
+        text = [re.sub("[^\w]", " ", sequence).split() for sequence in f]
+
+        return text
+
+    def _create_dict(self, words, chars):
+
+        word_index = {}
+        char_index = {}
+
+        for i, word in enumerate(words):
+            word_index[word] = i
+
+        for i, char in enumerate(chars):
+            char_index[char] = i+1
+        char_index[''] = 0
+
+        return word_index, char_index
+
+    def prepare_input_output(self, data):
+
+        output_words = []
+        input_words = []
+        input_chars = []
+        sequence_length = []
+
+
+        for sequence in data:
+            for i in range(len(sequence)-1):
+                output_words.append(sequence[i+1])
+                input_words.append(sequence[i])
+
+        for n in range(len(input_words)):
+            input_chars.append([c for c in input_words[n]])
+
+        return input_chars, output_words
+
+    def create_input_output(self, input_chars, output_words):
+        """
+        #In the paper we see that training is done in groups of seq_len=30 timesteps
+        """
+
+        inputs = []
+        outputs = []
+
+        seq_len = self.max_len
+
+        for n in range(0, len(input_chars)-seq_len, seq_len):
+            inputs.append(input_chars[n:n+seq_len])
+            outputs.append(output_words[n:n+seq_len])
+
+        return inputs, outputs
+
+def generator(inputs,
+             outputs,
+             batch_size,
+             max_len,
+             max_word_len,
+             word_index,
+             char_index):
+
+    I = 0
+    while True:
+        x = np.zeros(shape=(batch_size, max_len, max_word_len))
+        y = np.zeros(shape=(batch_size, max_len, len(word_index)))
+
+        for sample in range(batch_size):
+            for time in range(max_len):
+                if I >= len(inputs):
+                    I = 0
+                for chars in inputs[I]:
+                    for n, c in enumerate(chars):
+                        x[sample, time, n] =  char_index[c]
+
+                for word in outputs[I]:
+
+                    y[sample, time, word_index[word]] = 1.0
+
+                I +=1
+
+        yield x, y
