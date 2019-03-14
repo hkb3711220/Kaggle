825183095dd6458782180dc1d79708ae3e795217
unknown <h3711220@gmail.com>
2019-03-15 00:39:52 +0900

    make simplest End2End CNN for classifier

--- Pronoun/RobustedCoref/model.py
+++ Pronoun/RobustedCoref/model.py
@@ -2,49 +2,32 @@ from keras.layers import *
 from keras.models import *
 import keras.backend as K
 
+class Mention_Embedding(object):
 
-class MentionPairEmbeding(object):
-
-    def __init__(self, filters=280, max_len=50, embed_size=50, max_features=80000):
+    def __init__(self, filters=280, embed_size=300):
         self.filters = filters
         self.embed_size = embed_size
-        self.max_len = max_len
-        self.max_features = max_features
 
     def build(self):
+        P_Fea1 = Input(shape=(3, self.embed_size))  # Embedding of Parents, Mention, and Suceeding Word: String Features
+        P_Fea2 = Input(shape=(6, self.embed_size))  # Embeddings of 3 proceedings words, 3 succedings words of m
+        P_Fea3 = Input(shape=(3, self.embed_size))
+        Antecedent_Fea1 = Input(shape=(3, self.embed_size))
+        Antecedent_Fea2 = Input(shape=(6, self.embed_size))
+        Antecedent_Fea3 = Input(shape=(3, self.embed_size))
+        Dist_Fea = Input(shape=(2,))
 
-        M1 = Input(shape=(3, self.embed_size))
-        M2 = Input(shape=(6, self.embed_size))  # Embeddings of 3 proceedings words, 3 succedings words of m
-        M3 = Input(shape=(5, self.max_len * self.embed_size))  # 3 proceeding sentence, 1 succeding sentence, current sentence
-        A1 = Input(shape=(3, self.embed_size))
-        A2 = Input(shape=(6, self.embed_size))
-        A3 = Input(shape=(5, self.max_len * self.embed_size))
-        B1 = Input(shape=(3, self.embed_size))
-        B2 = Input(shape=(6, self.embed_size))
-        B3 = Input(shape=(5, self.max_len * self.embed_size))
+        Mention_Represent1 = self.mention_embed(P_Fea1, P_Fea2, P_Fea3, 'Mention')
+        Mention_Represent2 = self.mention_embed(Antecedent_Fea1, Antecedent_Fea2, Antecedent_Fea3, 'Antecedent')
 
-        Mention_embedding = self.mention_embed(M1, M2, M3)
-        A_embedding = self.mention_embed(A1, A2, A3)
-        B_embedding = self.mention_embed(B1, B2, B3)
+        x = self.mentionpair_embed(Mention_Represent1, Mention_Represent2)
+        x = Concatenate(name='Mention_Pair_Embedding')([x, Dist_Fea])
 
-        Mention_Pair1 = self.mentionpair_embed(Mention_embedding, A_embedding)
-        Mention_Pair2 = self.mentionpair_embed(Mention_embedding, B_embedding)
-        # Score_Layer
-        Linear = Dense(1, use_bias=True)
-        Mention_embedding = Flatten()(Mention_embedding)
-        ScoreM = Linear(Mention_embedding)
-        ScoreA = Linear(Mention_Pair1)
-        ScoreB = Linear(Mention_Pair2)
-
-        output = Concatenate()([ScoreA, ScoreB, ScoreM])
-        output = Activation('softmax')(output)
-
-        model = Model([M1, M2, M3, A1, A2, A3, B1, B2, B3], output)
+        model = Model([P_Fea1, P_Fea2, P_Fea3, Antecedent_Fea1, Antecedent_Fea2, Antecedent_Fea3, Dist_Fea], x)
 
         return model
 
-    def mention_embed(self, inp1, inp2, inp3):
-
+    def mention_embed(self, inp1, inp2, inp3, target):
         Conv1_fea1 = self.Conv1k(inp1, [1, 2, 3])
         Conv1_fea2 = self.Conv1k(inp2, [1, 2, 3])
         Conv1_fea3 = self.Conv1k(inp3, [1, 2, 3])
@@ -55,17 +38,9 @@ class MentionPairEmbeding(object):
         Conv1_fea3 = self.Expand_dim(Conv1_fea3)
         Conv2_Input = Concatenate(axis=1)([Conv1_fea1, Conv1_fea2, Conv1_fea3])
 
-        x = Conv2D(self.filters, kernel_size=(3, 3))(Conv2_Input)
-        x = Lambda(lambda x: K.squeeze(x, axis=1))(x)
-
-        return x
-
-    def mentionpair_embed(self, M1, M2):
-        x = Concatenate(axis=1)([M1, M2])
-        x = Conv1D(self.filters, kernel_size=2)(x)
-        x = MaxPool1D(pool_size=1)(x)
-        x = Flatten()(x)
-        x = Dense(self.filters, use_bias=True, activation='relu')(x)
+        x = Conv2D(self.filters, kernel_size=(3, 3), activation='tanh')(Conv2_Input)
+        x = MaxPool2D(pool_size=(1, 1))(x)
+        x = Lambda(lambda x: K.squeeze(x, axis=1), name="{}_Embed".format(target))(x)
 
         return x
 
@@ -74,10 +49,111 @@ class MentionPairEmbeding(object):
         convs = []
         shape = x.get_shape().as_list()
         for kernel in kernels:
-            conv = Conv1D(self.filters, kernel)(x)
+            conv = Conv1D(self.filters, kernel, activation='tanh')(x)
             pool = MaxPool1D(pool_size=int(shape[1] - kernel + 1))(conv)
+            pool = Dropout(0.8)(pool)
             convs.append(pool)
 
         convs = Concatenate(axis=1)(convs)
 
         return convs
+
+    def mentionpair_embed(self, M1, M2):
+        x = Concatenate(axis=1)([M1, M2])
+        x = Conv1D(self.filters, kernel_size=2, activation='tanh')(x)
+        x = MaxPool1D(pool_size=1)(x)
+        x = Dropout(0.8)(x)
+        x = Flatten()(x)
+
+        return x
+
+
+class Coreference_Classifier(object):
+
+    def __init__(self, Mention_Pair, Mention_Embedding, filters=60, embed_size=300):
+        self.filters = filters
+        self.embed_size = embed_size
+        self.Mention_Pair = Mention_Pair
+        self.Mention_Embed = Mention_Embedding
+        # self.customThresholdedReLU = customThresholdedReLU(theta=1.0, name="singleton_output")
+
+    def build(self):
+        M1 = Input(shape=(3, self.embed_size))  # Embedding of Parents, Mention, and Suceeding Word: String Features
+        M2 = Input(shape=(6, self.embed_size))  # Embeddings of 3 proceedings words, 3 succedings words of m
+        M3 = Input(shape=(3, self.embed_size))  # Average Embedding of 3 proceeding sentence, 1 succeding sentence, and current sentence
+        A1 = Input(shape=(3, self.embed_size))
+        A2 = Input(shape=(6, self.embed_size))
+        A3 = Input(shape=(3, self.embed_size))
+        B1 = Input(shape=(3, self.embed_size))
+        B2 = Input(shape=(6, self.embed_size))
+        B3 = Input(shape=(3, self.embed_size))
+        Dist_M_A = Input(shape=(2,))  # Mention and Antecedent A
+        Dist_M_B = Input(shape=(2,))  # Mention and Antecedent B
+
+        self.Expand_dim = Lambda(lambda x: K.expand_dims(x, axis=1))
+        Mention_Pair1 = self.Mention_Pair([M1, M2, M3, A1, A2, A3, Dist_M_A])
+        Mention_Pair2 = self.Mention_Pair([M1, M2, M3, B1, B2, B3, Dist_M_B])
+        Mention_embedding = self.Mention_Embed([M1, M2, M3])
+
+        Cluster_Embedding = self.CONVs(Mention_embedding)
+        M_Cluster_Embedding1 = self.CONVp(Mention_Pair1)
+        M_Cluster_Embedding2 = self.CONVp(Mention_Pair2)
+        # Mention_embedding = Flatten()(Mention_embedding)
+
+        output1 = Concatenate()([M_Cluster_Embedding1, M_Cluster_Embedding2, Cluster_Embedding])
+        output1 = Dense(self.filters, use_bias=True, activation='relu')(output1)
+        output1 = Dense(self.filters, use_bias=True, activation='relu')(output1)
+        output1 = Dense(3, use_bias=True, activation='softmax', name='cluster_output')(output1)
+
+        pair1 = self.cluster_classifier(Mention_Pair1, "pair1")
+        pair2 = self.cluster_classifier(Mention_Pair2, "pair2")
+        output2 = Add(name='singleton_output')([pair1, pair2])
+
+        model = Model([M1, M2, M3, A1, A2, A3, B1, B2, B3, Dist_M_A, Dist_M_B], [output1, output2])
+
+        return model
+
+    def CONVs(self, x):
+        x1 = GlobalAvgPool1D()(x)
+        x2 = GlobalMaxPool1D()(x)
+        x1 = self.Expand_dim(x1)
+        x2 = self.Expand_dim(x2)
+        x = Concatenate(axis=1)([x1, x2])
+        x = Conv1D(self.filters, kernel_size=2)(x)
+        x = MaxPool1D(pool_size=1)(x)
+        x = Flatten()(x)
+
+        return x
+
+    def CONVp(self, x):
+        x = self.Expand_dim(x)
+        x1 = GlobalAvgPool1D()(x)
+        x2 = GlobalMaxPool1D()(x)
+        x1 = self.Expand_dim(x1)
+        x2 = self.Expand_dim(x2)
+        x = Concatenate(axis=1)([x1, x2])
+        x = Conv1D(self.filters, kernel_size=2)(x)
+        x = MaxPool1D(pool_size=1)(x)
+        x = Flatten()(x)
+
+        return x
+
+    def cluster_classifier(self, x, _name):
+        x = Dense(self.filters, use_bias=True, activation='relu')(x)
+        x = Dense(1, use_bias=True, activation='sigmoid', name='{}_output'.format(_name))(x)
+
+        return x
+
+
+Embedding_model = Mention_Embedding().build()
+layer_name = 'Mention_Embed'
+Mention_Pair = Model(Embedding_model.inputs, Embedding_model.output)
+Mention_Embedding = Model([Embedding_model.inputs[0], Embedding_model.inputs[1], Embedding_model.inputs[2]],
+                          Embedding_model.get_layer(layer_name).output)
+
+model = Coreference_Classifier(Mention_Pair, Mention_Embedding).build()
+model.summary()
+model.compile(optimizer='adam',
+              loss={'cluster_output':'sparse_categorical_crossentropy', 'singleton_output':'mse'},
+              loss_weights={'cluster_output': 1.0, 'singleton_output': 1.0},
+              metrics={'cluster_output':"sparse_categorical_accuracy"})
--- Pronoun/loader.py
+++ Pronoun/loader.py
@@ -1,85 +1,70 @@
 import pandas as pd
 import numpy as np
 import os
-import nltk
 import spacy
-from nltk import Tree
 
 os.chdir(os.path.dirname(__file__))
 table = pd.read_table('./test_stage_1.tsv')
-
-def word_tags(mention):
-    """
-    the type of the mention
-    """
-    tags  = nltk.pos_tag(mention)
-    _, types = tags
-
-    return types
+nlp   = spacy.load('en_core_web_lg')
 
 def bs(lens, target):
-
-    low, high = 0, len(lens) -1
+    low, high = 0, len(lens) - 1
 
     while low < high:
         mid = low + int((high - low) / 2)
 
         if target > lens[mid]:
-            low = mid+1
+            low = mid + 1
         elif target < lens[mid]:
             high = mid
         else:
-            return mid+1
+            return mid + 1
 
     return low
 
-def to_nltk_tree(node):
-    if node.n_lefts + node.n_rights > 0:
-        return Tree(node.orth_, [to_nltk_tree(child) for child in node.children])
-    else:
-        return node.orth_
-
-class Embeding_features():
+class Mention_Features():
 
     def __init__(self):
 
-        self.nlp = spacy.load('en_core_web_lg')
+        self.nlp = nlp
 
     def create(self, charoffset, text):
 
         doc = self.nlp(text)
 
-        lens = [token.idx for token in doc] #The Charactor offset the token within the parent
-        mention_offset = bs(lens, charoffset) - 1 # The target in which index of tokens
-        mention = doc[mention_offset] #mention
+        lens = [token.idx for token in doc]  # The Charactor offset the token within the parent
+        mention_offset = bs(lens, charoffset) - 1  # The target in which index of tokens
+        mention = doc[mention_offset]  # mention
 
-        dependency_parent = mention.head #The syntactic parent, or "governor", of this token.
-        children = mention.children
+        dependency_parent = mention.head  # The syntactic parent, or "governor", of this token.
+        nbor = mention.nbor()  # The following word of nbor
 
-        sent_lens = [len(sent) for sent in doc.sents] #the sentence length
+        sent_lens = [len(sent) for sent in doc.sents]  # the sentence length
         acc_lens = sent_lens
         pre_lens = 0
         for i in range(0, len(sent_lens)):
             pre_lens += acc_lens[i]
             acc_lens[i] = pre_lens
-
-        sent_index = bs(acc_lens, mention_offset) #to Find out the charoffset in which sentence
+        sent_index = bs(acc_lens, mention_offset)  # to Find out the charoffset which sentence
         current_sent = list(doc.sents)[sent_index]
+        current_sent = [token for token in current_sent]
 
         preceding3 = self.n_preceding_words(3, doc, mention_offset)
         following3 = self.n_following_words(3, doc, mention_offset)
 
-        proceeding = []
-        for i in range(sent_index-2, sent_index):
-            if i < 0 :
-                continue
-            else:
-                proceeding.append(list(doc.sents)[i])
+        proceed_sents = [] # 3 proceeding sentence
+        for i in range(sent_index - 3, sent_index):
+            if i < 0: continue
+            proceeding = [token for token in list(doc.sents)[sent_index - 1]]
+            proceed_sents.extend(proceeding)
 
-        if sent_index+1 < len(list(doc.sents)):
-            succeeding = list(doc.sents)[sent_index+1]
+        if sent_index + 1 < len(list(doc.sents)): #1 succeeding sentence
+            succeeding = list(doc.sents)[sent_index + 1]
+            succeed_sent = [token for token in succeeding]
+        else:
+            succeed_sent = []
 
-        return mention, dependency_parent, preceding3, following3, proceeding, current_sent, succeeding
+        return mention, dependency_parent, nbor, preceding3, following3, proceed_sents, current_sent, succeed_sent
 
     def n_preceding_words(self, n, tokens, offset):
 
@@ -99,17 +84,11 @@ class Embeding_features():
 
         return followings
 
-mention, dependency_parent, childern, preceding3, following3, proceeding, current_sent, succeeding = Embeding_features().create(table["Pronoun-offset"][0], table["Text"][0])
-print(mention, dependency_parent, preceding3, following3, proceeding, current_sent, succeeding)
-
-
-class Distance_features():
+class Distance_Features():
 
     def __init__(self):
 
-        self.nlp = spacy.load('en_core_web_lg')
-        self.buckets = [1, 2, 3, 4, 5, 8, 16, 32, 64]
-        self.pos_buckets = [0, 1, 2, 3, 4, 5, 8, 16, 32]
+        self.nlp = nlp
 
     def create(self, char_offsetA, char_offsetB, text):
 
@@ -119,8 +98,8 @@ class Distance_features():
         mention_offsetA = bs(lens, char_offsetA) - 1
         mention_offsetB = bs(lens, char_offsetB) - 1
 
-        dist = mention_offsetA - mention_offsetB
-        dist_oh = self.one_hot(self.buckets, dist)
+        mention_dist = mention_offsetA - mention_offsetB
+        #dist_oh = self.one_hot(self.buckets, dist)
 
         sent_lens = [len(sent) for sent in doc.sents] #the sentence length
         acc_lens = sent_lens
@@ -134,102 +113,49 @@ class Distance_features():
 
         sent_dist = sentA_index - sentB_index
 
-        sentA = list(doc.sents)[sentA_index]
-        sentB = list(doc.sents)[sentB_index]
-        #print(doc.sents)
+        return [mention_dist, sent_dist]
 
-        posA = mention_offsetA + 1
-        if sentA_index > 0:
-            posA = mention_offsetA - acc_lens[sentA_index-1] #The Distance from first word to mention
-        posA_oh = self.one_hot(self.pos_buckets, posA)
-        posA_end = len(sentA) - posA #The Distance from last word to mention In sentence
-
-        posA_end_oh = self.one_hot(self.pos_buckets, posA_end)
-
-        posB = mention_offsetB + 1
-        if sentB_index > 0:
-            posB = mention_offsetB - acc_lens[sentB_index-1]
-        posB_oh = self.one_hot(self.pos_buckets, posB)
-        posB_end = len(sentB) - posB #The Distance from last word to mention
-        posB_end_oh = self.one_hot(self.pos_buckets, posB_end)
-
-        return dist_oh, posA_oh, posB_oh, posA_end_oh, posB_end_oh
-
-    def one_hot(self, lens, dist):
-
-        low, high = 0, len(lens)
-
-        while low < high:
-            mid = low + int((high-low) / 2)
-            if dist > lens[mid]:
-                low = mid + 1
-            elif dist < lens[mid]:
-                high = mid
-            else:
-                idx = mid
-                break
-
-        idx = low
-        if idx > len(lens) - 1:
-            idx = len(lens) - 1
-        one_hot = np.zeros(len(self.buckets))
-        one_hot[idx] = 1
-
-        return one_hot
-
-
-def extract_embed_features(df, text_column, offset_column, num_embed_features = 11, embed_dim = 300):
+def extract_embedding_features(df, text_column, offset_column,  embed_dim=300):
     text_offset_list = df[[text_column, offset_column]].values.tolist()
-    num_features     = num_embed_features
-    extractor        = Embeding_features()
+    extractor = Mention_Features()
 
-    embed_feature_matrix = np.zeros(shape=(len(text_offset_list), num_features, embed_dim))
+    feature_map1 = np.zeros(shape=(len(text_offset_list), 3, embed_dim))
+    feature_map2 = np.zeros(shape=(len(text_offset_list), 6, embed_dim))
+    feature_map3 = np.zeros(shape=(len(text_offset_list), 3, embed_dim))
+
     for text_offset_index in range(len(text_offset_list)):
         text_offset = text_offset_list[text_offset_index]
-        mention, parent, first_word, last_word, precedings2, followings2, precedings5, followings5, sent_tokens = extractor.create(text_offset[1], text_offset[0])
+        mention, dependency_parent, nbor, preceding3, following3, proceed_sents, current_sent, succeed_sent = extractor.create( text_offset[1], text_offset[0])
 
-        feature_index = 0
-        embed_feature_matrix[text_offset_index, feature_index, :] = mention.vector
-        feature_index += 1
-        embed_feature_matrix[text_offset_index, feature_index, :] = parent.vector
-        feature_index += 1
-        embed_feature_matrix[text_offset_index, feature_index, :] = first_word.vector
-        feature_index += 1
-        embed_feature_matrix[text_offset_index, feature_index, :] = last_word.vector
-        feature_index += 1
-        embed_feature_matrix[text_offset_index, feature_index:feature_index+2, :] = np.asarray([token.vector if token is not None else np.zeros((embed_dim,)) for token in precedings2])
-        feature_index += len(precedings2)
-        embed_feature_matrix[text_offset_index, feature_index:feature_index+2, :] = np.asarray([token.vector if token is not None else np.zeros((embed_dim,)) for token in followings2])
-        feature_index += len(followings2)
-        embed_feature_matrix[text_offset_index, feature_index, :] = np.mean(np.asarray([token.vector if token is not None else np.zeros((embed_dim,)) for token in precedings5]), axis=0)
-        feature_index += 1
-        embed_feature_matrix[text_offset_index, feature_index, :] = np.mean(np.asarray([token.vector if token is not None else np.zeros((embed_dim,)) for token in followings5]), axis=0)
-        feature_index += 1
-        embed_feature_matrix[text_offset_index, feature_index, :] = np.mean(np.asarray([token.vector for token in sent_tokens]), axis=0) if len(sent_tokens) > 0 else np.zeros(embed_dim)
-        feature_index += 1
+        # Feature Map1
+        feature_map1[text_offset_index, 0, :] = dependency_parent.vector
+        feature_map1[text_offset_index, 1, :] = mention.vector
+        feature_map1[text_offset_index, 2, :] = nbor.vector
 
-    return embed_feature_matrix
+        # Feature Map2
+        feature_map2[text_offset_index, 0:3, :] = np.asarray(
+            [token.vector if token is not None else np.zeros((embed_dim,)) for token in preceding3])
+        feature_map2[text_offset_index, 3:6, :] = np.asarray(
+            [token.vector if token is not None else np.zeros((embed_dim,)) for token in following3])
 
+        # Feature Map3
+        feature_map3[text_offset_index, 0, :] = np.mean(np.asarray([token.vector for token in proceed_sents]),
+                                                        axis=0) if len(proceed_sents) > 0 else np.zeros(embed_dim)
+        feature_map3[text_offset_index, 1, :] =  np.mean(np.asarray([token.vector for token in current_sent]),
+                                                         axis=0) if len(current_sent) > 0 else np.zeros(embed_dim)
+        feature_map3[text_offset_index, 2, :] =  np.mean(np.asarray([token.vector for token in succeed_sent]),
+                                                         axis=0) if len(succeed_sent) > 0 else np.zeros(embed_dim)
 
-def extract_dist_features(df, text_column, pronoun_offset_column, name_offset_column, num_features=45):
+    return feature_map1, feature_map2, feature_map3
+
+def extract_dist_features(df, text_column, pronoun_offset_column, name_offset_column):
     text_offset_list = df[[text_column, pronoun_offset_column, name_offset_column]].values.tolist()
-    extractor = Distance_features()
+    extractor = Distance_Features()
+    dist_feas = []
 
-    pos_feature_matrix = np.zeros(shape=(len(text_offset_list), num_features))
     for text_offset_index in range(len(text_offset_list)):
         text_offset = text_offset_list[text_offset_index]
-        dist_oh, sent_pos_oh1, sent_pos_oh2, sent_pos_inv_oh1, sent_pos_inv_oh2 = extractor.create(text_offset[1], text_offset[2], text_offset[0])
+        dist_fea = extractor.create(text_offset[1], text_offset[2], text_offset[0])
+        dist_feas.append(dist_fea)
 
-        feature_index = 0
-        pos_feature_matrix[text_offset_index, feature_index:feature_index+len(dist_oh)] = np.asarray(dist_oh)
-        feature_index += len(dist_oh)
-        pos_feature_matrix[text_offset_index, feature_index:feature_index+len(sent_pos_oh1)] = np.asarray(sent_pos_oh1)
-        feature_index += len(sent_pos_oh1)
-        pos_feature_matrix[text_offset_index, feature_index:feature_index+len(sent_pos_oh2)] = np.asarray(sent_pos_oh2)
-        feature_index += len(sent_pos_oh2)
-        pos_feature_matrix[text_offset_index, feature_index:feature_index+len(sent_pos_inv_oh1)] = np.asarray(sent_pos_inv_oh1)
-        feature_index += len(sent_pos_inv_oh1)
-        pos_feature_matrix[text_offset_index, feature_index:feature_index+len(sent_pos_inv_oh2)] = np.asarray(sent_pos_inv_oh2)
-        feature_index += len(sent_pos_inv_oh2)
-
-    return pos_feature_matrix
+    return np.asarray(dist_feas)
